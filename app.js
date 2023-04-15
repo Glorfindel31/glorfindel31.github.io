2deaa38358a301970fc193cafb432b18dd2499b5
glorfindel31 <116696371+Glorfindel31@users.noreply.github.com>
2023-04-15 11:16:19 +0700

    2nd commit

--- app.js
+++ app.js
@@ -8,3 +8,41 @@ let gameMatrix = [
   [0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0],
 ];
+document.addEventListener('DOMContentLoaded', () => {
+  let gameState = 0;
+
+  for (let i = 0; i < gameMatrix.length; i++) {
+    for (let j = 0; j < gameMatrix[i].length; j++) {
+      let cell = document.createElement('div');
+      cell.classList.add('grid');
+      cell.dataset.x = i;
+      cell.dataset.y = j;
+      cell.state = 0;
+      document.querySelector('.board-container').append(cell);
+    }
+  }
+  const renderToken = () => {
+    for (let i = 0; i < gameMatrix.length; i++) {
+      for (let j = 0; j < gameMatrix[i].length; j++) {
+        if (gameMatrix[i][j] === 1) {
+          document
+            .querySelector(`[data-x="${i}"][data-y="${j}"]`)
+            .classList.add('token1');
+        }
+      }
+    }
+  };
+  document.querySelectorAll('.grid').forEach((cell) => {
+    cell.addEventListener('click', (e) => {
+      if (gameState === 0) {
+        for (let i = 0; i < gameMatrix.length; i++) {
+          for (let j = 0; j < gameMatrix[i].length; j++) {
+            if (e.target.dataset['y'] === j.toString()) {
+              console.log(e.target.dataset['y']);
+            }
+          }
+        }
+      }
+    });
+  });
+});
